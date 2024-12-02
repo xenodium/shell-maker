5bad1dfbb367e5fc6cd30dfb30d32035850f5ea7
xenodium <me+gh@xenodium.com>
2024-12-02 20:52:14 +0000

    Bumping version

--- shell-maker.el
+++ shell-maker.el
@@ -4,7 +4,7 @@
 
 ;; Author: Alvaro Ramirez https://xenodium.com
 ;; URL: https://github.com/xenodium/shell-maker
-;; Version: 0.72.1
+;; Version: 0.73.1
 ;; Package-Requires: ((emacs "27.1"))
 
 ;; This package is free software; you can redistribute it and/or modify
