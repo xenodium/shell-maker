0e6004e3b0d5aad87538368a6f3fb05f1618df6d
xenodium <me+gh@xenodium.com>
2025-12-10 21:10:26 +0000

    Bumping version

--- shell-maker.el
+++ shell-maker.el
@@ -4,7 +4,7 @@
 
 ;; Author: Alvaro Ramirez https://xenodium.com
 ;; URL: https://github.com/xenodium/shell-maker
-;; Version: 0.84.2
+;; Version: 0.84.3
 ;; Package-Requires: ((emacs "27.1"))
 
 ;; This package is free software; you can redistribute it and/or modify
@@ -32,7 +32,7 @@
 
 ;;; Code:
 
-(defconst shell-maker-version "0.84.2")
+(defconst shell-maker-version "0.84.3")
 
 (require 'comint)
 (require 'goto-addr)
